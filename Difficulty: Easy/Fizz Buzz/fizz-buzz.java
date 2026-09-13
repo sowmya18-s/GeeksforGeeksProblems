69309d083206f91d0daf6580bd76c46102c1972c
sowmya gudavalli <sowmyagudavalli17@gmail.com>
2026-09-14 00:26:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Fizz Buzz/fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static ArrayList<String> fizzBuzz(int n) {
+        ArrayList<String> arr=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0){
+                arr.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                arr.add("Fizz");
+            }
+            else if(i%5==0){
+                arr.add("Buzz");
+            }
+            else{
+                arr.add(String.valueOf(i));
+            }
+        }
+        return arr;
+    }
+}
