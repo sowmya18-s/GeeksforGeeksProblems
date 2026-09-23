62cf390d4713d7a50cfb4c13c077b32cb602e2b8
sowmya gudavalli <sowmyagudavalli17@gmail.com>
2026-09-23 23:35:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fighting the Darkness/fighting-the-darkness.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxDays(int arr[]) {
+        // code here
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            max=Math.max(max,arr[i]);
+        }
+        return max;
+    }
+}
